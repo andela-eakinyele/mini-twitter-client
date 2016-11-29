e0bf2eeae645586cbd9432f13ce9af8f5caec9e2
Emmanuel Akinyele <emmanuel.akinyele@andela.com>
2016-11-29 11:32:06 +0100

    Fix error in server code

--- server/index.js
+++ server/index.js
@@ -4,10 +4,23 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
 const request = require('request');
+const passport = require('passport');
+const TwitterStrategy = require('passport-twitter');
 
 require('dotenv').config();
 
 const app = express();
+
+passport.use(new TwitterStrategy({
+	consumerKey:  process.env.CONSUMER_KEY,
+	consumerSecret: process.env.CONSUMER_SECRET,
+	callbackUrl: 'http://localhost:9000/auth/twitter/callback',
+	passReqToCallback: true
+}, function(req, token, tokenSecret, profile, done){
+
+	done(null);
+}));
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,15 +32,23 @@ app.use(session({
 	secret: process.env.SECRET
 }));
 
+
+app.use(passport.initialize());
+
 app.use(express.static(path.resolve(__dirname, '../public')));
+
+// app.get('/auth/twitter', passport.authenticate('twitter'));
+
+// app.get('/auth/twitter/callback', function(req, res) {
+// 	res.redirect('/');
+// });
 
 app.post('/status/tweet', (req, res) => {
 	const body = req.body;
 
-	console.log(process.env.CONSUMER_KEY, process.env.TOKEN_SECRET)
 	const oauth = {
 		consumer_key: process.env.CONSUMER_KEY,
-		comsumer_secret: process.env.CONSUMER_SECRET,
+		consumer_secret: process.env.CONSUMER_SECRET,
 		token: process.env.TOKEN,
 		token_secret: process.env.TOKEN_SECRET
 	};
@@ -39,7 +60,6 @@ app.post('/status/tweet', (req, res) => {
 	const url = 'https://api.twitter.com/1.1/statuses/update.json';
 
 	request.post({ url, oauth, qs, json: true }, (err, response, body) => {
-		console.log(err, response.statusCode, body);
 		if (err) {
 			if (response.statusCode === 401) {
 				return res.status(401).json({ error: 'Failed Authentication' });
